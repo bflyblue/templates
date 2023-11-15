b3df3838614e2c5b98e3d31d24a8813c61a275a8
Shaun Sharples <shaun.sharples@gmail.com>
2023-11-15 10:01:13 +0200

    add ghc96

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
         path = ./ghc94;
         description = "Empty GHC 9.4 project";
       };
+      ghc96 = {
+        path = ./ghc96;
+        description = "Empty GHC 9.6 project";
+      };
       servant-typescript = {
         path = ./servant-typescript;
         description = "Servant webserver + typescript website";
