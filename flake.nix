5006af4d57a78ceff21abf3c278efed69bfcdd2d
Shaun Sharples <shaun.sharples@gmail.com>
2025-12-23 20:10:02 +0200

    ghc 9.12

--- flake.nix
+++ flake.nix
@@ -14,15 +14,19 @@
         description = "Empty GHC 9.8 project";
       };
       ghc910 = {
-        path = ./ghc98;
+        path = ./ghc910;
         description = "Empty GHC 9.10 project";
+      };
+      ghc912 = {
+        path = ./ghc912;
+        description = "Empty GHC 9.12 project";
       };
       servant-typescript = {
         path = ./servant-typescript;
         description = "Servant webserver + typescript website";
       };
 
-      default = self.templates.ghc98;
+      default = self.templates.ghc912;
     };
   };
 }
---
+++ ghc912/flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "TEMPLATE";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+      forallSystems =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f (rec {
+            inherit system;
+            pkgs = nixpkgsFor system;
+            haskellPackages = hpkgsFor system pkgs;
+          })
+        );
+      nixpkgsFor = system: import nixpkgs { inherit system; };
+      hpkgsFor =
+        system: pkgs:
+        with pkgs.haskell.lib;
+        pkgs.haskell.packages.ghc912.override {
+          overrides = self: super: {
+        }; };
+    in
+    {
+      packages = forallSystems (
+        {
+          system,
+          pkgs,
+          haskellPackages,
+        }:
+        {
+          TEMPLATE = haskellPackages.callCabal2nix "TEMPLATE" ./. { };
+          default = self.packages.${system}.TEMPLATE;
+        }
+      );
+      devShells = forallSystems (
+        {
+          system,
+          pkgs,
+          haskellPackages,
+        }:
+        {
+          TEMPLATE = haskellPackages.shellFor {
+            packages = p: [ self.packages.${system}.TEMPLATE ];
+            buildInputs = with haskellPackages; [
+              cabal-install
+              haskell-language-server
+            ];
+            withHoogle = true;
+          };
+          default = self.devShells.${system}.TEMPLATE;
+        }
+      );
+    };
+}
